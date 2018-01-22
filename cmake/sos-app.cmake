024df3b61d6ed3ff99e836fd32cd42e17c370eba
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2018-01-22 12:40:17 -0700

    bug fixes, cmake sets definitions rather than just flags

--- cmake/sos-app.cmake
+++ cmake/sos-app.cmake
@@ -2,24 +2,24 @@
 set(CMAKE_VERBOSE_MAKEFILE TRUE CACHE INTERNAL "verbose make")
 
 if( ${SOS_APP_ARCH} STREQUAL armv7-m )
-	set(BUILD_NAME "build_release_armv7m" CACHE INTERNAL "build name")
-	set(BUILD_DEVICE "armv7m" CACHE INTERNAL "build device")
-	set(BUILD_FLOAT ${TOOLCHAIN_FLOAT_DIR_ARMV7M} CACHE INTERNAL "build float")
-	set(BUILD_FLOAT_OPTIONS "" CACHE INTERNAL "build float options")
+	set(BUILD_NAME "build_release_armv7m")
+	set(BUILD_DEVICE "armv7m")
+	set(BUILD_FLOAT ${TOOLCHAIN_FLOAT_DIR_ARMV7M})
+	set(BUILD_FLOAT_OPTIONS "")
 else()
-	set(BUILD_NAME "build_release_armv7em_fpu" CACHE INTERNAL "build name")
-	set(BUILD_FLOAT "${TOOLCHAIN_FLOAT_DIR_ARMV7EM_FPU}" CACHE INTERNAL "build float dir")
-	set(BUILD_DEVICE "armv7em" CACHE INTERNAL "build device")
-	set(BUILD_FLOAT_OPTIONS ${TOOLCHAIN_FLOAT_OPTIONS_ARMV7EM_FPU} CACHE INTERNAL "build float options")
+	set(BUILD_NAME "build_release_armv7em_fpu")
+	set(BUILD_FLOAT "${TOOLCHAIN_FLOAT_DIR_ARMV7EM_FPU}")
+	set(BUILD_DEVICE "armv7em")
+	set(BUILD_FLOAT_OPTIONS ${TOOLCHAIN_FLOAT_OPTIONS_ARMV7EM_FPU})
 endif()
 
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/${BUILD_NAME} CACHE INTERNAL "runtime output")
 
 file(MAKE_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
 
-set(BUILD_LIBRARIES ${SOS_APP_LIBRARIES} api sos_crt CACHE INTERNAL "sos build libraries")
-set(BUILD_FLAGS -march=${SOS_APP_ARCH} -D__${BUILD_DEVICE} ${BUILD_FLOAT_OPTIONS} CACHE INTERNAL "sos app build options")
-set(LINKER_FLAGS "-L${TOOLCHAIN_LIB_DIR}/${SOS_APP_ARCH}/${BUILD_FLOAT}/. -Wl,-Map,${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${SOS_APP_NAME}_${SOS_APP_ARCH}.map,--defsym=_app_ram_size=${SOS_APP_RAM_SIZE},--gc -Tldscripts/app.ld -nostdlib -u crt" CACHE INTERNAL "sos app linker options")
+set(BUILD_LIBRARIES ${SOS_APP_LIBRARIES} api sos_crt)
+set(BUILD_FLAGS -mlong-calls -march=${SOS_APP_ARCH} ${BUILD_FLOAT_OPTIONS})
+set(LINKER_FLAGS "-L${TOOLCHAIN_LIB_DIR}/${SOS_APP_ARCH}/${BUILD_FLOAT}/. -Wl,-Map,${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${SOS_APP_NAME}_${SOS_APP_ARCH}.map,--defsym=_app_ram_size=${SOS_APP_RAM_SIZE},--gc -Tldscripts/app.ld -nostdlib -u crt")
 
 add_executable(${SOS_APP_NAME}_${SOS_APP_ARCH}.elf ${SOS_APP_SOURCELIST})
 add_custom_target(bin_${SOS_APP_NAME}_${SOS_APP_ARCH} DEPENDS ${SOS_APP_NAME}_${SOS_APP_ARCH}.elf COMMAND ${CMAKE_OBJCOPY} -j .text -j .data -O binary ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${SOS_APP_NAME}_${SOS_APP_ARCH}.elf ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${SOS_APP_NAME})
@@ -27,6 +27,7 @@ add_custom_target(asm_${SOS_APP_NAME}_${SOS_APP_ARCH} DEPENDS bin_${SOS_APP_NAME
 add_custom_target(size_${SOS_APP_ARCH} DEPENDS asm_${SOS_APP_NAME}_${SOS_APP_ARCH} COMMAND ${CMAKE_SIZE} ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${SOS_APP_NAME}_${SOS_APP_ARCH}.elf)
 add_custom_target(${SOS_APP_ARCH} ALL DEPENDS size_${SOS_APP_ARCH})
 
+target_compile_definitions(${SOS_APP_NAME}_${SOS_APP_ARCH}.elf __${BUILD_DEVICE} ${SOS_APP_DEFINITIONS})
 target_link_libraries(${SOS_APP_NAME}_${SOS_APP_ARCH}.elf ${BUILD_LIBRARIES})
 set_target_properties(${SOS_APP_NAME}_${SOS_APP_ARCH}.elf PROPERTIES LINK_FLAGS ${LINKER_FLAGS})
 target_compile_options(${SOS_APP_NAME}_${SOS_APP_ARCH}.elf PUBLIC ${BUILD_FLAGS})
