0dc79665fb35556609b817b7198c960b29a34e1a
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-07-25 12:42:22 -0600

    cmake updates

--- cmake/sos-app-cmakelists.cmake
+++ cmake/sos-app-cmakelists.cmake
@@ -2,11 +2,25 @@
 cmake_minimum_required (VERSION 3.6)
 
 #Grabs the SOS CMAKE SDK from the default install location -- update as needed
-if( ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Darwin" )
-	set(SOS_TOOLCHAIN_CMAKE_PATH /Applications/StratifyLabs-SDK/Tools/gcc/arm-none-eabi/cmake)
-elseif( ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows" )
-	set(SOS_TOOLCHAIN_CMAKE_PATH C:/StratifyLabs-SDK/Tools/gcc/arm-none-eabi/cmake)
+# Grab the SDK location
+if(NOT DEFINED SOS_SDK_PATH)
+	if(DEFINED ENV{SOS_SDK_PATH})
+		set(SOS_SDK_PATH $ENV{SOS_SDK_PATH})
+	elseif( ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Darwin" )
+		message(WARNING "default SDK locations are deprecated")
+		set(SOS_SDK_PATH /Applications/StratifyLabs-SDK)
+	elseif( ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows" )
+		message(WARNING "default SDK locations are deprecated")
+		set(SOS_SDK_PATH C:/StratifyLabs-SDK)
+	elseif( ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Linux" )
+		message(WARNING "default SDK locations are deprecated")
+		set(SOS_SDK_PATH /StratifyLabs-SDK)
+	endif()
 endif()
+if(NOT DEFINED SOS_SDK_PATH)
+	message(FATAL_ERROR "Please set environment variable SOS_SDK_PATH to location of the StratifyLabs-SDK directory")
+endif()
+set(SOS_TOOLCHAIN_CMAKE_PATH ${SOS_SDK_PATH}/Tools/gcc/arm-none-eabi/cmake)
 
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-variables.cmake)
 
