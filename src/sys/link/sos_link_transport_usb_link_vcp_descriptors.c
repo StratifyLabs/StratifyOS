5b6456a9f246f6c64a8696130a5c22a6461a37d7
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-11-05 15:04:47 -0700

    formatting

--- src/sys/link/sos_link_transport_usb_link_vcp_descriptors.c
+++ src/sys/link/sos_link_transport_usb_link_vcp_descriptors.c
@@ -14,26 +14,25 @@
  * You should have received a copy of the GNU General Public License
  * along with Stratify OS.  If not, see <http://www.gnu.org/licenses/>. */
 
-#include <errno.h>
-#include <string.h>
-#include <stdbool.h>
-#include <sys/fcntl.h>
-#include <unistd.h>
 #include "config.h"
-#include "sos/link.h"
-#include "mcu/mcu.h"
-#include "mcu/boot_debug.h"
-#include "sos/dev/usb.h"
+#include "device/sys.h"
 #include "device/usbfifo.h"
-#include "usbd/control.h"
-#include "usbd/cdc.h"
+#include "mcu/boot_debug.h"
 #include "mcu/core.h"
 #include "mcu/debug.h"
-#include "device/sys.h"
+#include "mcu/mcu.h"
+#include "sos/dev/usb.h"
+#include "sos/link.h"
+#include "usbd/cdc.h"
+#include "usbd/control.h"
+#include <errno.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/fcntl.h>
+#include <unistd.h>
 
 #include "sos/link/transport_usb_link_vcp.h"
 #include "sos/link/transport_usb_vcp.h"
-
 
 #define SOS_REQUIRED_CURRENT 500
 
@@ -44,54 +43,50 @@
 #define VCP0_INTERFACE_STRING 5
 
 typedef struct MCU_PACK {
-	usbd_msft_compatible_id_header_feature_descriptor_t header;
-	usbd_msft_compatible_id_interface_feature_descriptor_t interface_feature0;
-	usbd_msft_compatible_id_interface_feature_descriptor_t interface_feature1;
-	//usbd_msft_compatible_id_interface_feature_descriptor_t interface_feature2;
+  usbd_msft_compatible_id_header_feature_descriptor_t header;
+  usbd_msft_compatible_id_interface_feature_descriptor_t interface_feature0;
+  usbd_msft_compatible_id_interface_feature_descriptor_t interface_feature1;
+  // usbd_msft_compatible_id_interface_feature_descriptor_t interface_feature2;
 } msft_os1_compatible_id_feature_descriptor_t;
 
-static const msft_os1_compatible_id_feature_descriptor_t msft_os1_compatible_id_feature_descriptor =
-{
-	.header = {
-		.length = sizeof(msft_os1_compatible_id_feature_descriptor_t),
-		.bcd = 0x0100,
-		.compatible_id_index = 0x0004,
-		.section_count[0] = 1,
-	},
-	.interface_feature0 = {
-		.interface_number = 0,
-		.resd0 = 0x01,
-		.compatible_id = {'W', 'I', 'N', 'U', 'S', 'B', 0x00, 0x00}, //WINUSB\0\0
-	}
-	,
-	.interface_feature1 = {
-		.interface_number = 1,
-		.resd0 = 0x01
-	}
-		#if 0
+static const msft_os1_compatible_id_feature_descriptor_t
+  msft_os1_compatible_id_feature_descriptor = {
+    .header =
+      {
+        .length = sizeof(msft_os1_compatible_id_feature_descriptor_t),
+        .bcd = 0x0100,
+        .compatible_id_index = 0x0004,
+        .section_count[0] = 2,
+      },
+    .interface_feature0 =
+      {
+        .interface_number = 0,
+        .resd0 = 0x01,
+        .compatible_id = {'W', 'I', 'N', 'U', 'S', 'B', 0x00, 0x00}, // WINUSB\0\0
+      },
+    .interface_feature1 = {.interface_number = 1, .resd0 = 0x01}
+#if 0
 	,
 	.interface_feature2 = {
 		.interface_number = 2,
 		.resd0 = 0x01
 	}
-		#endif
+#endif
 };
 
 typedef struct MCU_PACK {
-	usbd_msft_os2_function_subset_header_t header_descriptor;
-	usbd_msft_os2_compatible_id_t compatible_id_descriptor;
+  usbd_msft_os2_function_subset_header_t header_descriptor;
+  usbd_msft_os2_compatible_id_t compatible_id_descriptor;
 } interface_descriptor_t;
 
-
 typedef struct MCU_PACK {
-	usbd_msft_os2_configuration_subset_header_t header_descriptor;
-	interface_descriptor_t interface_descriptor;
+  usbd_msft_os2_configuration_subset_header_t header_descriptor;
+  interface_descriptor_t interface_descriptor;
 } configuration_descriptor_t;
 
-
 typedef struct MCU_PACK {
-	usbd_msft_os2_descriptor_set_header_t header_descriptor;
-	configuration_descriptor_t configuration_descriptor;
+  usbd_msft_os2_descriptor_set_header_t header_descriptor;
+  configuration_descriptor_t configuration_descriptor;
 } compatible_id_feature_descriptor_t;
 
 #if 0
@@ -128,56 +123,49 @@ static const compatible_id_feature_descriptor_t msft_compatible_id_feature_descr
 #endif
 
 const usbd_msft_bos_descriptor_t bos_descriptor = {
-	.bos_descriptor = {
-		.bLength = sizeof(usbd_bos_descriptor_t),
-		.bDescriptorType = USBD_DESCRIPTOR_TYPE_BOS,
-		.wTotalLength = sizeof(usbd_msft_bos_descriptor_t),
-		.bNumDeviceCaps = 1
-	},
-	.bLength = sizeof(usbd_msft_bos_descriptor_t) - sizeof(usbd_bos_descriptor_t),
-	.bDescriptorType = 0x10,
-	.bDevCapabilityType = 0x05,
-	.bReserved = 0,
-	.PlatformCapabilityUUID = {
-		0xDF, 0x60, 0xDD, 0xD8,
-		0x89, 0x45, 0xC7, 0x4C,
-		0x9C, 0xD2, 0x65, 0x9D,
-		0x9E, 0x64, 0x8A, 0x9F
-	},
-	.dwWindowsVersion = 0x06030000,
-	.wMSOSDescriptorSetTotalLength = sizeof(compatible_id_feature_descriptor_t),
-	.bMS_VendorCode = SOS_LINK_TRANSPORT_MSFT_VENDOR_CODE,
-	.bAltEnumCode = 0
-};
+  .bos_descriptor =
+    {.bLength = sizeof(usbd_bos_descriptor_t),
+     .bDescriptorType = USBD_DESCRIPTOR_TYPE_BOS,
+     .wTotalLength = sizeof(usbd_msft_bos_descriptor_t),
+     .bNumDeviceCaps = 1},
+  .bLength = sizeof(usbd_msft_bos_descriptor_t) - sizeof(usbd_bos_descriptor_t),
+  .bDescriptorType = 0x10,
+  .bDevCapabilityType = 0x05,
+  .bReserved = 0,
+  .PlatformCapabilityUUID =
+    {0xDF, 0x60, 0xDD, 0xD8, 0x89, 0x45, 0xC7, 0x4C, 0x9C, 0xD2, 0x65, 0x9D, 0x9E, 0x64,
+     0x8A, 0x9F},
+  .dwWindowsVersion = 0x06030000,
+  .wMSOSDescriptorSetTotalLength = sizeof(compatible_id_feature_descriptor_t),
+  .bMS_VendorCode = SOS_LINK_TRANSPORT_MSFT_VENDOR_CODE,
+  .bAltEnumCode = 0};
 
-int link_vcp_class_handler(void * object, const mcu_event_t * event){
-	usbd_control_t * context = object;
-	u32 o_events = event->o_events;
+int link_vcp_class_handler(void *object, const mcu_event_t *event) {
+  usbd_control_t *context = object;
+  u32 o_events = event->o_events;
 
-	if( sos_link_transport_usb_msft_string_event(object,event) ){
-		mcu_debug_printf("send MSFT100\n");
-		return 1;
-	}
+  if (sos_link_transport_usb_msft_string_event(object, event)) {
+    mcu_debug_printf("send MSFT100\n");
+    return 1;
+  }
 
-	if( (o_events & MCU_EVENT_FLAG_SETUP)
-			&& (context->setup_packet.bRequest == USBD_MSFT_VENDOR_CODE_BYTE)
-			&& (context->setup_packet.wIndex.w == 0x0004)){
+  if (
+    (o_events & MCU_EVENT_FLAG_SETUP)
+    && (context->setup_packet.bRequest == USBD_MSFT_VENDOR_CODE_BYTE)
+    && (context->setup_packet.wIndex.w == 0x0004)) {
 
+    mcu_debug_printf(
+      "request 0x%X 0x%X 0x%X\n", context->setup_packet.bmRequestType,
+      context->setup_packet.bRequest, context->setup_packet.wValue.w);
 
-		mcu_debug_printf("request 0x%X 0x%X 0x%X\n",
-										 context->setup_packet.bmRequestType,
-										 context->setup_packet.bRequest,
-										 context->setup_packet.wValue.w);
-
-		u16 len = sizeof(msft_os1_compatible_id_feature_descriptor);
-		context->data.dptr = (u8*)&msft_os1_compatible_id_feature_descriptor;
-		if (context->data.nbyte > len) {
-			context->data.nbyte = len;
-		}
-		mcu_debug_printf("send OS descriptors %d\n", len);
-		usbd_control_datain_stage(context);
-		return 1;
-
+    u16 len = sizeof(msft_os1_compatible_id_feature_descriptor);
+    context->data.dptr = (u8 *)&msft_os1_compatible_id_feature_descriptor;
+    if (context->data.nbyte > len) {
+      context->data.nbyte = len;
+    }
+    mcu_debug_printf("send OS descriptors %d\n", len);
+    usbd_control_datain_stage(context);
+    return 1;
 
 #if 0
 		if( context->setup_packet.bRequest == SOS_LINK_TRANSPORT_MSFT_VENDOR_CODE ){
@@ -204,69 +192,74 @@ int link_vcp_class_handler(void * object, const mcu_event_t * event){
 			return 1;
 		}
 #endif
-	}
+  }
 
-	return sos_link_usbd_cdc_event_handler(object, event);
+  return sos_link_usbd_cdc_event_handler(object, event);
 }
 
-SOS_LINK_TRANSPORT_USB_DEVICE_DESCRIPTOR(link_vcp,239,2,1,SOS_LINK_TRANSPORT_USB_BCD_VERSION | 2)
+SOS_LINK_TRANSPORT_USB_DEVICE_DESCRIPTOR(
+  link_vcp,
+  239,
+  2,
+  1,
+  SOS_LINK_TRANSPORT_USB_BCD_VERSION | 2)
 
 SOS_LINK_TRANSPORT_USB_CONST(
-		link_vcp,
-		SOS_LINK_TRANSPORT_USB_PORT,
-		0,
-		0,
-		link_vcp_class_handler
-		)
-
+  link_vcp,
+  SOS_LINK_TRANSPORT_USB_PORT,
+  0,
+  0,
+  link_vcp_class_handler)
 
 const sos_link_transport_usb_link_vcp_configuration_descriptor_t
-sos_link_transport_usb_link_vcp_configuration_descriptor MCU_WEAK = {
+  sos_link_transport_usb_link_vcp_configuration_descriptor MCU_WEAK = {
 
-		.cfg = {
-		.bLength = sizeof(usbd_configuration_descriptor_t),
-		.bDescriptorType = USBD_DESCRIPTOR_TYPE_CONFIGURATION,
-		.wTotalLength = sizeof(sos_link_transport_usb_link_vcp_configuration_descriptor_t)-1, //exclude the zero terminator
-		.bNumInterfaces = 3,
-		.bConfigurationValue = 0x01,
-		.iConfiguration = 2,
-		.bmAttributes = USBD_CONFIGURATION_ATTRIBUTES_BUS_POWERED,
-		.bMaxPower = USBD_CONFIGURATION_MAX_POWER_MA( SOS_REQUIRED_CURRENT )
-		},
+    .cfg =
+      {.bLength = sizeof(usbd_configuration_descriptor_t),
+       .bDescriptorType = USBD_DESCRIPTOR_TYPE_CONFIGURATION,
+       .wTotalLength = sizeof(sos_link_transport_usb_link_vcp_configuration_descriptor_t)
+                       - 1, // exclude the zero terminator
+       .bNumInterfaces = 3,
+       .bConfigurationValue = 0x01,
+       .iConfiguration = 2,
+       .bmAttributes = USBD_CONFIGURATION_ATTRIBUTES_BUS_POWERED,
+       .bMaxPower = USBD_CONFIGURATION_MAX_POWER_MA(SOS_REQUIRED_CURRENT)},
 
-		.link_interface = {
-		SOS_LINK_TRANSPORT_USB_DECLARE_INTERFACE_CONFIGURATION_DESCRIPTOR(
-			INTERFACE_STRING,
-			INTERFACE_NUMBER,
-			SOS_LINK_TRANSPORT_USB_BULK_ENDPOINT,
-			SOS_LINK_TRANSPORT_USB_BULK_ENDPOINT_SIZE
-			)
-		},
+    .link_interface = {SOS_LINK_TRANSPORT_USB_DECLARE_INTERFACE_CONFIGURATION_DESCRIPTOR(
+      INTERFACE_STRING,
+      INTERFACE_NUMBER,
+      SOS_LINK_TRANSPORT_USB_BULK_ENDPOINT,
+      SOS_LINK_TRANSPORT_USB_BULK_ENDPOINT_SIZE)},
 
-		.vcp = {
-		USBD_CDC_DECLARE_CONFIGURATION_DESCRIPTOR(
-			VCP0_INTERFACE_STRING,
-			VCP0_INTERFACE_STRING,
-			VCP0_INTERFACE,
-			VCP0_INTERFACE+1,
-			SOS_LINK_TRANSPORT_USB_LINK_VCP_INTERRUPT_ENDPOINT,
-			SOS_LINK_TRANSPORT_USB_LINK_VCP_INTERRUPT_ENDPOINT_SIZE,
-			SOS_LINK_TRANSPORT_USB_LINK_VCP_BULK_ENDPOINT,
-			SOS_LINK_TRANSPORT_USB_LINK_VCP_BULK_ENDPOINT_SIZE
-			)
-		},
+    .vcp = {USBD_CDC_DECLARE_CONFIGURATION_DESCRIPTOR(
+      VCP0_INTERFACE_STRING,
+      VCP0_INTERFACE_STRING,
+      VCP0_INTERFACE,
+      VCP0_INTERFACE + 1,
+      SOS_LINK_TRANSPORT_USB_LINK_VCP_INTERRUPT_ENDPOINT,
+      SOS_LINK_TRANSPORT_USB_LINK_VCP_INTERRUPT_ENDPOINT_SIZE,
+      SOS_LINK_TRANSPORT_USB_LINK_VCP_BULK_ENDPOINT,
+      SOS_LINK_TRANSPORT_USB_LINK_VCP_BULK_ENDPOINT_SIZE)},
 
-		.terminator = 0
-		};
+    .terminator = 0};
 
-const struct sos_link_transport_usb_link_vcp_string_descriptor_t sos_link_transport_usb_link_vcp_string_descriptor MCU_WEAK = {
-	.bLength = 4,
-	.bDescriptorType = USBD_DESCRIPTOR_TYPE_STRING,
-	.wLANGID = 0x0409, //English
-	.manufacturer = USBD_ASSIGN_STRING(SOS_LINK_TRANSPORT_USB_DESC_MANUFACTURER_SIZE, SOS_LINK_TRANSPORT_USB_DESC_MANUFACTURER_STRING),
-	.product = USBD_ASSIGN_STRING(SOS_LINK_TRANSPORT_USB_DESC_PRODUCT_SIZE, SOS_LINK_TRANSPORT_USB_DESC_PRODUCT_STRING),
-	.serial = USBD_ASSIGN_STRING(SOS_LINK_TRANSPORT_USB_DESC_SERIAL_SIZE, 0), //dynamically load SN based on silicon
-	.link = USBD_ASSIGN_STRING(SOS_LINK_TRANSPORT_USB_DESCRIPTOR_SOS_LINK_SIZE, SOS_LINK_TRANSPORT_USB_DESCRIPTOR_SOS_LINK),
-	.vcp = USBD_ASSIGN_STRING(SOS_LINK_TRANSPORT_LINK_USB_VCP_DESCRIPTOR_SIZE, SOS_LINK_TRANSPORT_LINK_USB_VCP_DESCRIPTOR)
-};
-
+const struct sos_link_transport_usb_link_vcp_string_descriptor_t
+  sos_link_transport_usb_link_vcp_string_descriptor MCU_WEAK = {
+    .bLength = 4,
+    .bDescriptorType = USBD_DESCRIPTOR_TYPE_STRING,
+    .wLANGID = 0x0409, // English
+    .manufacturer = USBD_ASSIGN_STRING(
+      SOS_LINK_TRANSPORT_USB_DESC_MANUFACTURER_SIZE,
+      SOS_LINK_TRANSPORT_USB_DESC_MANUFACTURER_STRING),
+    .product = USBD_ASSIGN_STRING(
+      SOS_LINK_TRANSPORT_USB_DESC_PRODUCT_SIZE,
+      SOS_LINK_TRANSPORT_USB_DESC_PRODUCT_STRING),
+    .serial = USBD_ASSIGN_STRING(
+      SOS_LINK_TRANSPORT_USB_DESC_SERIAL_SIZE,
+      0), // dynamically load SN based on silicon
+    .link = USBD_ASSIGN_STRING(
+      SOS_LINK_TRANSPORT_USB_DESCRIPTOR_SOS_LINK_SIZE,
+      SOS_LINK_TRANSPORT_USB_DESCRIPTOR_SOS_LINK),
+    .vcp = USBD_ASSIGN_STRING(
+      SOS_LINK_TRANSPORT_LINK_USB_VCP_DESCRIPTOR_SIZE,
+      SOS_LINK_TRANSPORT_LINK_USB_VCP_DESCRIPTOR)};
