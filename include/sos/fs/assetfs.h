72563da49b5365689560402004617c9cc44e94e6
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-12-08 15:23:22 -0700

    formatting

--- include/sos/fs/assetfs.h
+++ include/sos/fs/assetfs.h
@@ -22,89 +22,78 @@
 #include "../link/types.h"
 #include "sys/dirent.h"
 
-int assetfs_init(const void* cfg);
-int assetfs_startup(const void* cfg);
-int assetfs_open(const void* cfg, void ** handle, const char * path, int flags, int mode);
-int assetfs_read(const void * cfg, void * handle, int flags, int loc, void * buf, int nbyte);
-int assetfs_ioctl(const void * cfg, void * handle, int request, void * ctl);
-int assetfs_close(const void* cfg, void ** handle);
-int assetfs_fstat(const void* cfg, void * handle, struct stat * st);
-int assetfs_stat(const void* cfg, const char * path, struct stat * st);
-int assetfs_opendir(const void* cfg, void ** handle, const char * path);
-int assetfs_readdir_r(const void* cfg, void * handle, int loc, struct dirent * entry);
-int assetfs_closedir(const void* cfg, void ** handle);
+int assetfs_init(const void *cfg);
+int assetfs_startup(const void *cfg);
+int assetfs_open(const void *cfg, void **handle, const char *path, int flags, int mode);
+int assetfs_read(const void *cfg, void *handle, int flags, int loc, void *buf, int nbyte);
+int assetfs_ioctl(const void *cfg, void *handle, int request, void *ctl);
+int assetfs_close(const void *cfg, void **handle);
+int assetfs_fstat(const void *cfg, void *handle, struct stat *st);
+int assetfs_stat(const void *cfg, const char *path, struct stat *st);
+int assetfs_opendir(const void *cfg, void **handle, const char *path);
+int assetfs_readdir_r(const void *cfg, void *handle, int loc, struct dirent *entry);
+int assetfs_closedir(const void *cfg, void **handle);
 
-#define ASSETFS_FILE(name, file) \
-	__asm__(".section .rodata\n" \
-	".global assetfs_incbin_" #name "_start\n" \
-	".type assetfs_incbin_" #name "_start, %object\n" \
-	".balign 16\n" \
-	"assetfs_incbin_" #name "_start:\n" \
-	".incbin \"" file "\"\n" \
-	\
-	".global assetfs_incbin_" #name "_end\n" \
-	".type assetfs_incbin_" #name "_end, %object\n" \
-	".balign 1\n" \
-	"assetfs_incbin_" #name "_end:\n" \
-	".byte 0\n" \
-	); \
-	extern const __attribute__((aligned(16))) void* assetfs_incbin_##name##_start; \
-	extern const void* assetfs_incbin_##name##_end
+#define ASSETFS_FILE(name, file)                                                         \
+  __asm__(                                                                               \
+    ".section .rodata\n"                                                                 \
+    ".global assetfs_incbin_" #name                                                      \
+    "_start\n"                                                                           \
+    ".type assetfs_incbin_" #name                                                        \
+    "_start, %object\n"                                                                  \
+    ".balign 16\n"                                                                       \
+    "assetfs_incbin_" #name                                                              \
+    "_start:\n"                                                                          \
+    ".incbin \"" file                                                                    \
+    "\"\n"                                                                               \
+                                                                                         \
+    ".global assetfs_incbin_" #name                                                      \
+    "_end\n"                                                                             \
+    ".type assetfs_incbin_" #name                                                        \
+    "_end, %object\n"                                                                    \
+    ".balign 1\n"                                                                        \
+    "assetfs_incbin_" #name                                                              \
+    "_end:\n"                                                                            \
+    ".byte 0\n");                                                                        \
+  extern const __attribute__((aligned(16))) void *assetfs_incbin_##name##_start;         \
+  extern const void *assetfs_incbin_##name##_end
 
 #define ASSETFS_START(name) (&assetfs_incbin_##name##_start)
 #define ASSETFS_END(name) (&assetfs_incbin_##name##_end)
 
-#define ASSETFS_ENTRY(file_name, object_name, mode_value, uid_value) \
-{ .name = file_name, .start = ASSETFS_START(object_name), .end = ASSETFS_END(object_name), .mode = mode_value, .uid = uid_value }
+#define ASSETFS_ENTRY(file_name, object_name, mode_value, uid_value)                     \
+  {                                                                                      \
+    .name = file_name, .start = ASSETFS_START(object_name),                              \
+    .end = ASSETFS_END(object_name), .mode = mode_value, .uid = uid_value                \
+  }
 
 typedef struct MCU_PACK {
-	char name[LINK_NAME_MAX];
-	u32 start;
-	u32 end;
-	u16 uid;
-	u16 mode;
+  char name[LINK_NAME_MAX];
+  u32 start;
+  u32 end;
+  u16 uid;
+  u16 mode;
 } assetfs_dirent_t;
 
 typedef struct {
-	u32 count;
-	const assetfs_dirent_t entries[];
+  u32 count;
+  const assetfs_dirent_t entries[];
 } assetfs_config_t;
 
-
-#define ASSETFS_MOUNT(mount_loc_name, cfgp, permissions_value, owner_value) { \
-	.mount_path = mount_loc_name, \
-	.permissions = permissions_value, \
-	.owner = owner_value, \
-	.mount = assetfs_init, \
-	.unmount = SYSFS_NOTSUP, \
-	.ismounted = sysfs_always_mounted, \
-	.startup = assetfs_startup, \
-	.mkfs = SYSFS_NOTSUP, \
-	.open = assetfs_open, \
-	.aio = SYSFS_NOTSUP, \
-	.read = assetfs_read, \
-	.write = SYSFS_NOTSUP, \
-	.close = assetfs_close, \
-	.ioctl = assetfs_ioctl, \
-	.rename = SYSFS_NOTSUP, \
-	.fsync = SYSFS_NOTSUP, \
-	.unlink = SYSFS_NOTSUP, \
-	.mkdir = SYSFS_NOTSUP, \
-	.rmdir = SYSFS_NOTSUP, \
-	.remove = SYSFS_NOTSUP, \
-	.opendir = assetfs_opendir, \
-	.closedir = assetfs_closedir, \
-	.readdir_r = assetfs_readdir_r, \
-	.link = SYSFS_NOTSUP, \
-	.symlink = SYSFS_NOTSUP, \
-	.stat = assetfs_stat, \
-	.lstat = SYSFS_NOTSUP, \
-	.fstat = assetfs_fstat, \
-	.chmod = SYSFS_NOTSUP, \
-	.chown = SYSFS_NOTSUP, \
-	.unlock = SYSFS_NOTSUP_VOID, \
-	.config = cfgp, \
-	}
-
+#define ASSETFS_MOUNT(mount_loc_name, cfgp, permissions_value, owner_value)              \
+  {                                                                                      \
+    .mount_path = mount_loc_name, .permissions = permissions_value,                      \
+    .owner = owner_value, .mount = assetfs_init, .unmount = SYSFS_NOTSUP,                \
+    .ismounted = sysfs_always_mounted, .startup = assetfs_startup, .mkfs = SYSFS_NOTSUP, \
+    .open = assetfs_open, .aio = SYSFS_NOTSUP, .read = assetfs_read,                     \
+    .write = SYSFS_NOTSUP, .close = assetfs_close, .ioctl = assetfs_ioctl,               \
+    .rename = SYSFS_NOTSUP, .fsync = SYSFS_NOTSUP, .unlink = SYSFS_NOTSUP,               \
+    .mkdir = SYSFS_NOTSUP, .rmdir = SYSFS_NOTSUP, .remove = SYSFS_NOTSUP,                \
+    .opendir = assetfs_opendir, .closedir = assetfs_closedir,                            \
+    .readdir_r = assetfs_readdir_r, .link = SYSFS_NOTSUP, .symlink = SYSFS_NOTSUP,       \
+    .stat = assetfs_stat, .lstat = SYSFS_NOTSUP, .fstat = assetfs_fstat,                 \
+    .chmod = SYSFS_NOTSUP, .chown = SYSFS_NOTSUP, .unlock = SYSFS_NOTSUP_VOID,           \
+    .config = cfgp,                                                                      \
+  }
 
 #endif /* SOS_FS_ASSETFS_H_ */
--- include/sos/fs/drive_assetfs.h
+++ include/sos/fs/drive_assetfs.h
@@ -17,86 +17,74 @@
 #ifndef SOS_FS_DRIVE_ASSETFS_H_
 #define SOS_FS_DRIVE_ASSETFS_H_
 
-#include <sdk/types.h>
-#include "sos/dev/drive.h"
 #include "../link/types.h"
+#include "sos/dev/drive.h"
 #include "sysfs.h"
+#include <sdk/types.h>
 
-int drive_assetfs_init(const void* cfg);
-int drive_assetfs_exit(const void* cfg);
-int drive_assetfs_startup(const void* cfg);
-int drive_assetfs_open(const void* cfg, void ** handle, const char * path, int flags, int mode);
-int drive_assetfs_read(const void * cfg, void * handle, int flags, int loc, void * buf, int nbyte);
-int drive_assetfs_ioctl(const void * cfg, void * handle, int request, void * ctl);
-int drive_assetfs_close(const void* cfg, void ** handle);
-int drive_assetfs_fstat(const void* cfg, void * handle, struct stat * st);
-int drive_assetfs_stat(const void* cfg, const char * path, struct stat * st);
-int drive_assetfs_opendir(const void* cfg, void ** handle, const char * path);
-int drive_assetfs_readdir_r(const void* cfg, void * handle, int loc, struct dirent * entry);
-int drive_assetfs_closedir(const void* cfg, void ** handle);
+int drive_assetfs_init(const void *cfg);
+int drive_assetfs_exit(const void *cfg);
+int drive_assetfs_startup(const void *cfg);
+int drive_assetfs_open(
+  const void *cfg,
+  void **handle,
+  const char *path,
+  int flags,
+  int mode);
+int drive_assetfs_read(
+  const void *cfg,
+  void *handle,
+  int flags,
+  int loc,
+  void *buf,
+  int nbyte);
+int drive_assetfs_ioctl(const void *cfg, void *handle, int request, void *ctl);
+int drive_assetfs_close(const void *cfg, void **handle);
+int drive_assetfs_fstat(const void *cfg, void *handle, struct stat *st);
+int drive_assetfs_stat(const void *cfg, const char *path, struct stat *st);
+int drive_assetfs_opendir(const void *cfg, void **handle, const char *path);
+int drive_assetfs_readdir_r(const void *cfg, void *handle, int loc, struct dirent *entry);
+int drive_assetfs_closedir(const void *cfg, void **handle);
 
 typedef struct MCU_PACK {
-	char name[LINK_NAME_MAX];
-	u32 start;
-	u32 size;
-	u16 uid;
-	u16 mode;
+  char name[LINK_NAME_MAX];
+  u32 start;
+  u32 size;
+  u16 uid;
+  u16 mode;
 } drive_assetfs_dirent_t;
 
-
 typedef struct {
-	u32 count;
-	const drive_assetfs_dirent_t entries[];
+  u32 count;
+  const drive_assetfs_dirent_t entries[];
 } drive_assetfs_header_t;
-
 
 #if !defined __link
 typedef struct {
-	sysfs_shared_config_t drive;
-	u32 offset;
+  sysfs_shared_config_t drive;
+  u32 offset;
 } drive_assetfs_config_t;
 
 typedef struct {
-	sysfs_shared_state_t drive;
+  sysfs_shared_state_t drive;
 } drive_assetfs_state_t;
 
-
-#define DRIVE_ASSETFS_MOUNT(mount_loc_name, cfgp, permissions_value, owner_value) { \
-	.mount_path = mount_loc_name, \
-	.permissions = permissions_value, \
-	.owner = owner_value, \
-	.mount = drive_assetfs_init, \
-	.unmount = drive_assetfs_exit, \
-	.ismounted = sysfs_always_mounted, \
-	.startup = drive_assetfs_startup, \
-	.mkfs = SYSFS_NOTSUP, \
-	.open = drive_assetfs_open, \
-	.aio = SYSFS_NOTSUP, \
-	.read = drive_assetfs_read, \
-	.write = SYSFS_NOTSUP, \
-	.close = drive_assetfs_close, \
-	.ioctl = drive_assetfs_ioctl, \
-	.rename = SYSFS_NOTSUP, \
-	.fsync = SYSFS_NOTSUP, \
-	.unlink = SYSFS_NOTSUP, \
-	.mkdir = SYSFS_NOTSUP, \
-	.rmdir = SYSFS_NOTSUP, \
-	.remove = SYSFS_NOTSUP, \
-	.opendir = drive_assetfs_opendir, \
-	.closedir = drive_assetfs_closedir, \
-	.readdir_r = drive_assetfs_readdir_r, \
-	.link = SYSFS_NOTSUP, \
-	.symlink = SYSFS_NOTSUP, \
-	.stat = drive_assetfs_stat, \
-	.lstat = SYSFS_NOTSUP, \
-	.fstat = drive_assetfs_fstat, \
-	.chmod = SYSFS_NOTSUP, \
-	.chown = SYSFS_NOTSUP, \
-	.unlock = SYSFS_NOTSUP_VOID, \
-	.config = cfgp, \
-	}
+#define DRIVE_ASSETFS_MOUNT(mount_loc_name, cfgp, permissions_value, owner_value)        \
+  {                                                                                      \
+    .mount_path = mount_loc_name, .permissions = permissions_value,                      \
+    .owner = owner_value, .mount = drive_assetfs_init, .unmount = drive_assetfs_exit,    \
+    .ismounted = sysfs_always_mounted, .startup = drive_assetfs_startup,                 \
+    .mkfs = SYSFS_NOTSUP, .open = drive_assetfs_open, .aio = SYSFS_NOTSUP,               \
+    .read = drive_assetfs_read, .write = SYSFS_NOTSUP, .close = drive_assetfs_close,     \
+    .ioctl = drive_assetfs_ioctl, .rename = SYSFS_NOTSUP, .fsync = SYSFS_NOTSUP,         \
+    .unlink = SYSFS_NOTSUP, .mkdir = SYSFS_NOTSUP, .rmdir = SYSFS_NOTSUP,                \
+    .remove = SYSFS_NOTSUP, .opendir = drive_assetfs_opendir,                            \
+    .closedir = drive_assetfs_closedir, .readdir_r = drive_assetfs_readdir_r,            \
+    .link = SYSFS_NOTSUP, .symlink = SYSFS_NOTSUP, .stat = drive_assetfs_stat,           \
+    .lstat = SYSFS_NOTSUP, .fstat = drive_assetfs_fstat, .chmod = SYSFS_NOTSUP,          \
+    .chown = SYSFS_NOTSUP, .unlock = SYSFS_NOTSUP_VOID, .config = cfgp,                  \
+  }
 
 #endif
-
 
 #endif /* SOS_FS_DRIVE_ASSETFS_H_ */
