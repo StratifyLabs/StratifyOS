c73d511217f00672318480197290ea7b55c8817f
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2021-07-13 18:54:25 -0600

    Remove debug items, cherry-pick to v4.0 if committed there

--- src/device/ffifo.c
+++ src/device/ffifo.c
@@ -191,7 +191,6 @@ int ffifo_getinfo(
       info->frame_count - atomic_position.access.tail + atomic_position.access.head;
   }
 
-  sos_debug_printf("%p count ready %d / %d\n", state, info->frame_count_ready, info->frame_count);
   info->o_flags = state->o_flags;
   state->o_flags &= ~FIFO_FLAG_IS_OVERFLOW; // clear the overflow since it has been read
   return 0;
@@ -376,13 +375,11 @@ int ffifo_write_local(
 
   // writes need to be a integer multiple of the frame size
   if ((async->nbyte % config->frame_size) != 0) {
-    sos_debug_printf("bad write size\n");
     bytes_written = SYSFS_SET_RETURN(EINVAL);
   } else {
     bytes_written = ffifo_write_buffer(
       config, state, async->buf_const,
       async->nbyte); // see if there are bytes in the buffer
-    sos_debug_printf("wrote %d bytes\n", bytes_written);
     if (bytes_written == 0) {
       if (async->flags & O_NONBLOCK) {
         bytes_written = SYSFS_SET_RETURN(EAGAIN);
