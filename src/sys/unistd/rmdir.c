9ed3624486f9bd01a4cd6d567807c71bdc287ef1
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2021-08-06 13:44:59 -0600

    Formatting updates

--- src/sys/unistd/rmdir.c
+++ src/sys/unistd/rmdir.c
@@ -1,14 +1,13 @@
 // Copyright 2011-2021 Tyler Gilbert and Stratify Labs, Inc; see LICENSE.md
 
-
 /*! \addtogroup unistd
  * @{
  */
 
 /*! \file */
 
-#include "unistd_local.h"
 #include "unistd_fs.h"
+#include "unistd_local.h"
 
 /*! \details This function removes the directory specified by \a path.
  *
@@ -19,23 +18,21 @@
  * - ENOTEMPTY:  \a path is not an empty directory
  *
  */
-int rmdir(const char *path){
-	const sysfs_t * fs;
+int rmdir(const char *path) {
+  const sysfs_t *fs;
 
-	if ( sysfs_ispathinvalid(path) == true ){
-		return -1;
-	}
+  if (sysfs_ispathinvalid(path) == true) {
+    return -1;
+  }
 
-	fs = sysfs_find(path, true);
-	if ( fs != NULL ){
-        int ret = fs->rmdir(fs->config,
-				sysfs_stripmountpath(fs, path)
-				);
-        SYSFS_PROCESS_RETURN(ret);
-        return ret;
-	}
-	errno = ENOENT;
-	return -1;
+  fs = sysfs_find(path, true);
+  if (fs != NULL) {
+    int ret = fs->rmdir(fs->config, sysfs_stripmountpath(fs, path));
+    SYSFS_PROCESS_RETURN(ret);
+    return ret;
+  }
+  errno = ENOENT;
+  return -1;
 }
 
 /*! @} */
